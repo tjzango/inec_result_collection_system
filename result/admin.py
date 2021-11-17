72d5d07445dbec00fb2f88f3152dcb849db110f4
Haruna Tijjani Zango <tjzango@gmail.com>
2021-11-17 08:36:45 +0100

    Put models in admin

--- result/admin.py
+++ result/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import (
+    Lga,
+    States,
+    Party,
+    Ward,
+    AnnouncedWardResults,
+    PollingUnit,
+    Agentname,
+    AnnouncedPuResults,
+    AnnouncedLgaResults,
+    AnnouncedStateResults,
+)
 # Register your models here.
+
+admin.site.register(States)
+admin.site.register(Lga)
+admin.site.register(PollingUnit)
+admin.site.register(Party)
+admin.site.register(Ward)
+admin.site.register(AnnouncedStateResults)
+admin.site.register(AnnouncedLgaResults)
+admin.site.register(Agentname)
+admin.site.register(AnnouncedPuResults)
+admin.site.register(AnnouncedWardResults)
