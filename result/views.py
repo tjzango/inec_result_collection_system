a3b8b55752376be965633118fbbe866fe6b828cd
Haruna Tijjani Zango <tjzango@gmail.com>
2021-11-17 10:53:31 +0100

    Index page mokeup

--- result/views.py
+++ result/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import (
+    Lga,
+    States,
+    Party,
+    Ward,
+    AnnouncedWardResults,
+    PollingUnit,
+    Agentname,
+    AnnouncedPuResults,
+    AnnouncedLgaResults,
+    AnnouncedStateResults,
+)
 
 # Create your views here.
 class BasePage(View):
     
     def get(self, request, **kwarg):
+        context = {
+            'polling_units': AnnouncedPuResults.objects.all()
+        }
         return render(request,"home.html")
