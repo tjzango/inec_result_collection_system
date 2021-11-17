f21984abc661c8679dbc454101281b6e1d050d74
Haruna Tijjani Zango <tjzango@gmail.com>
2021-11-17 10:23:11 +0100

    Created Base Files and default reder

--- result/views.py
+++ result/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class BasePage(View):
+    
+    def get(self, request, **kwarg):
+        return render(request,"home.html")
