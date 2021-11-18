ec7db9c18a212242462ec31d14c2157dd71e03dd
Haruna Tijjani Zango <tjzango@gmail.com>
2021-11-18 10:11:37 +0100

    Urls update

--- result/views.py
+++ result/views.py
@@ -34,9 +34,9 @@ class LgaResultsView(View):
         }
         context['lgas'] = Lga.objects.all()
         if key != "":
-            data = AnnouncedPuResults.objects.filter(uniqueid=key)
+            data = AnnouncedPuResults.objects.filter(polling_unit_uniqueid=key)
             context["lga_results"] = data        
             return render(request,"lga.html", context)
         
-        context['polling_units_relults'] = AnnouncedPuResults.objects.all(),
+        
         return render(request,"lga.html", context)
